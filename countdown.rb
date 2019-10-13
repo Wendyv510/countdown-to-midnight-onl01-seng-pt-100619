b98ddfca881fab3e9aa532cc7c07c05b6b57ed3e
Wendy Harris <wendyv510@hotmail.com>
2019-10-13 18:31:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
-#write your code here
+counter = 10 #write your code here
 
 def countdown
+  while counter >0 
+  puts "#{counter} SECONDS!"
+  counter -=1 
+  counter ==0 
+  puts "HAPPY NEW YEAR!"
 end
